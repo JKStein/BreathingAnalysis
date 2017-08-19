8da56e4f64dba6a2d957a6c50ba4894a5a338162
lmuguhl <mail@jonas-stein.de>
2017-08-19 02:11:25 +0200

    generalized the metronome and cleaned up the main class and the metronome

--- app/src/main/java/com/jonas/breathinganalysis/BreathingAnalysis.java
+++ app/src/main/java/com/jonas/breathinganalysis/BreathingAnalysis.java
@@ -16,9 +16,6 @@ import java.util.ArrayList;
 
 import static android.content.ContentValues.TAG;
 
-//import com.jonas.breathinganalysis.Deprecated.DBChartManager;
-//import com.jonas.breathinganalysis.Deprecated.DBMeasurement;
-
 public class BreathingAnalysis extends Activity{
 
     ArrayList<SensorDate> accelerationSensorValues, rotationSensorValues, magnetSensorValues;
@@ -26,23 +23,13 @@ public class BreathingAnalysis extends Activity{
     ArrayList<Long> percussionEventValues;
 
     AccelerationRecorder accelerationRecorder;
-    //AccelerationChartManager accelerationChartManager;
-
     RotationRecorder rotationRecorder;
-
     MagnetRecorder magnetRecorder;
-
-    //DBMeasurement dbMeasurement;
-    //DBChartManager dbChartManager;
 
     private SensorManager sensorManager;
     private Sensor accelerometer, gyroscope, magnetometer;
 
     private Button measurementController;
-
-    //private MediaPlayer mediaPlayer;
-    //private long metronomeStartTimestamp;
-    //private long metronomeEndTimestamp;
 
     SoundPool soundPool;
     int tick, tock;
@@ -52,68 +39,34 @@ public class BreathingAnalysis extends Activity{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         accelerationSensorValues = new ArrayList<>();
         rotationSensorValues = new ArrayList<>();
         magnetSensorValues = new ArrayList<>();
         soundEventValues = new ArrayList<>();
         percussionEventValues = new ArrayList<>();
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         accelerationRecorder = new AccelerationRecorder(this);
         rotationRecorder = new RotationRecorder(this);
         magnetRecorder = new MagnetRecorder(this);
-        //accelerationChartManager = new AccelerationChartManager(this);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        new AudioHandler(this);
+        initializeAndRegisterAccelerometer();
+        initializeAndRegisterGyroscope();
+        initializeAndRegisterMagnetometer();
 
-        installAccelerometer();
-        installGyroscope();
-        installMagnetometer();
+        new AudioHandler(this);
 
         soundPool = new SoundPool.Builder().build();
         tick = soundPool.load(this, R.raw.tick,1);
-        //tick = soundPool.load(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)+ "/tick.ogg",1);
         tock = soundPool.load(this, R.raw.tock,1);
         handler = new Handler();
-        metronome = new Metronome(soundPool, tick, tock, handler, this);
+        metronome = new Metronome(soundPool, tick, tock, handler, this, 80, 3, 5);
         bestFittingStartTimestamp = 0;
 
-
-        /*File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-        final File metronomeAudio = new File(path, "metronome.mp3");
-        mediaPlayer = MediaPlayer.create(this, Uri.parse(metronomeAudio.getPath()));
-        final BreathingAnalysis breathingAnalysis = this;
-
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            public void onCompletion(MediaPlayer mp) {
-                metronomeEndTimestamp = System.currentTimeMillis() - 500;
-                System.out.println("metronomeEndTimestamp: " + metronomeEndTimestamp);
-                (new DataHandler(new MeasuredData(accelerationSensorValues, rotationSensorValues,  magnetSensorValues, soundEventValues, percussionEventValues))).start();
-                measurementController.setText(R.string.start);
-                mediaPlayer.reset();
-                try {
-                    mediaPlayer.setDataSource(breathingAnalysis, Uri.parse(metronomeAudio.getPath()));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    mediaPlayer.prepare();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });*/
-
-        //dbMeasurement = new DBMeasurement(this);
-        //dbChartManager = new DBChartManager(this);
-        //dbMeasurement.initializeDBMeasurement();
-        //dbMeasurement.startRecorder();
-
-        //audio processing
         installButton();
     }
 
@@ -122,16 +75,6 @@ public class BreathingAnalysis extends Activity{
         measurementController.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if(measurementController.getText().equals("Start")) {
-                    /*mediaPlayer.start();
-                    long mediaPlayerStartTimestamp = System.currentTimeMillis();*/
-                    /*while(mediaPlayer.getCurrentPosition() < 100) {
-                        System.out.println("Metronome did not make sufficient progress (" + mediaPlayer.getCurrentPosition() + ") yet!");
-                    }*/
-                    /*metronomeStartTimestamp = mediaPlayerStartTimestamp + 700;
-                    System.out.println("metronomeStartTimestamp: " + metronomeStartTimestamp);
-                    idunno();*/
-
-
                     handler.post(metronome);
 
                     accelerationSensorValues.clear();
@@ -139,12 +82,10 @@ public class BreathingAnalysis extends Activity{
                     magnetSensorValues.clear();
                     soundEventValues.clear();
                     measurementController.setText(R.string.stop);
-
                 }
                 else {
                     metronome.reset();
                     handler.removeCallbacks(metronome);
-                    (new DataHandler(new MeasuredData(accelerationSensorValues, rotationSensorValues,  magnetSensorValues, soundEventValues, percussionEventValues, bestFittingStartTimestamp))).start();
                     measurementController.setText(R.string.start);
                 }
 
@@ -152,53 +93,14 @@ public class BreathingAnalysis extends Activity{
         });
     }
 
-    /*private void idunno() {
-        ScheduledExecutorService myScheduledExecutorService = Executors.newScheduledThreadPool(1);
-
-        final Handler monitorHandler = new Handler(){
-
-            @Override
-            public void handleMessage(Message msg) {
-                mediaPlayerMonitor();
-            }
-
-        };
-
-        myScheduledExecutorService.scheduleWithFixedDelay(
-                new Runnable(){
-                    @Override
-                    public void run() {
-                        monitorHandler.sendMessage(monitorHandler.obtainMessage());
-                    }},
-                200, //initialDelay
-                200, //delay
-                TimeUnit.MILLISECONDS);
-
-    }
-
-
-    private void mediaPlayerMonitor(){
-        if (mediaPlayer != null) {
-            if(mediaPlayer.isPlaying()){
-                int mediaDuration = mediaPlayer.getDuration();
-                int mediaPosition = mediaPlayer.getCurrentPosition();
-                System.out.println("Duration: " + mediaDuration);
-                System.out.println("Progress: " + mediaPosition);
-            }
-        }
-    }*/
-
 
     void setBestFittingStartTimestamp(long bestFittingStartTimestamp) {
-        System.out.println("Hello! The best fitting start time stamp would be is: " + bestFittingStartTimestamp);
-        this.bestFittingStartTimestamp = bestFittingStartTimestamp;
+        (new DataHandler(new MeasuredData(accelerationSensorValues, rotationSensorValues,  magnetSensorValues, soundEventValues, percussionEventValues, bestFittingStartTimestamp))).start();
         measurementController.performClick();
     }
 
-    /**
-     * Initializes
-     */
-    private void installAccelerometer() {
+
+    private void initializeAndRegisterAccelerometer() {
         if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             sensorManager.registerListener(accelerationRecorder, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
@@ -208,7 +110,7 @@ public class BreathingAnalysis extends Activity{
         }
     }
 
-    private void installGyroscope() {
+    private void initializeAndRegisterGyroscope() {
         if (sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
             gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
             sensorManager.registerListener(rotationRecorder, gyroscope, SensorManager.SENSOR_DELAY_FASTEST);
@@ -218,7 +120,7 @@ public class BreathingAnalysis extends Activity{
         }
     }
 
-    private void installMagnetometer() {
+    private void initializeAndRegisterMagnetometer() {
         if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
             magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
             sensorManager.registerListener(magnetRecorder, magnetometer, SensorManager.SENSOR_DELAY_FASTEST);
